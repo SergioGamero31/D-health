86fba4ece12c9ef5ba524e4586fcc928015f9ffe
SergioGamero31 <64720452+SergioGamero31@users.noreply.github.com>
2021-07-11 14:38:08 -0500

    podometro act2

--- app/src/main/java/Fragments/PedometerFragment.java
+++ app/src/main/java/Fragments/PedometerFragment.java
@@ -1,83 +1,77 @@
 package Fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.diabetes.R;
-import com.example.diabetes.SignupActivity;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 
 public class PedometerFragment extends Fragment implements SensorEventListener {
 
-    View view;
-
-    private TextView stepsTaken;
+    private TextView txstepCounter;
     private CircularProgressBar cProgressBar;
     private Button btnReset;
 
     private SensorManager sensorManager;
-    private Sensor stepCounter;
+    private Sensor mstepCounter;
 
-    private boolean cSensorPresent;
+    private boolean sensorPresent;
     int stepCount = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_podometer, container, false);
+        if(ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
 
-        cProgressBar = (CircularProgressBar) view.findViewById(R.id.cProgressBar);
-        stepsTaken = (TextView) view.findViewById(R.id.stepsTaken);
-        btnReset = (Button) view.findViewById(R.id.btnReset);
+        View view = inflater.inflate(R.layout.fragment_podometer, container, false);
+
+        txstepCounter = view.findViewById(R.id.txstepCounter);
+        cProgressBar = view.findViewById(R.id.cProgressBar);
+        btnReset = view.findViewById(R.id.btnReset);
 
         sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
 
         if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
-            stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            cSensorPresent = true;
+            mstepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            sensorPresent = true;
         }else {
-            Toast.makeText(getActivity(), "No se encontró el sensor", Toast.LENGTH_SHORT).show();
-            cSensorPresent = false;
+            Toast.makeText(getActivity(), "No se encontró el sensor", Toast.LENGTH_LONG).show();
+            sensorPresent = false;
         }
 
+        resetSteps();
         return view;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
-            sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
-            sensorManager.unregisterListener(this, stepCounter);
-        }
-    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(event.sensor == stepCounter){
-            stepCount =(int) event.values[0];
-            stepsTaken.setText(String.valueOf(stepCount));
+        if(event.sensor == mstepCounter){
+            stepCount = (int) event.values[0];
+            txstepCounter.setText(String.valueOf(stepCount));
+            cProgressBar.setProgressWithAnimation((float) stepCount);
+
         }
     }
 
@@ -86,7 +80,27 @@ public class PedometerFragment extends Fragment implements SensorEventListener {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.registerListener(this, mstepCounter, SensorManager.SENSOR_DELAY_FASTEST);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.unregisterListener(this, mstepCounter);
+    }
 
-
+    public void resetSteps(){
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stepCount = 0;
+                txstepCounter.setText(String.valueOf(stepCount));
+            }
+        });
+    }
 }
